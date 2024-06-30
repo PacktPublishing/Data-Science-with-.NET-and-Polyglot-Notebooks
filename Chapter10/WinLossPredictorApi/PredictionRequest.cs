d324650925d082f09b5c9c40646c04651871f41e
Matt Eland <Matt.Eland@GMail.com>
2024-06-30 14:13:36 -0400

    Added Files for WinLossPredictorApi.

--- Chapter10/WinLossPredictorApi/PredictionRequest.cs
+++ Chapter10/WinLossPredictorApi/PredictionRequest.cs
@@ -4,6 +4,8 @@ public class PredictionRequest
 {
     public float OurPosition { get; set; }
     public float OpponentPosition { get; set; }
+    public float OurGoalsLast3Games {get; set;}
+    public float OpponentGoalsLast3Games {get; set;}
     public bool IsHome { get; set; }
     public string Label {get; set;}
 }
